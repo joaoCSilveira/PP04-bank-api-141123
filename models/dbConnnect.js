de87a3665e426482a1d8d0233a2347fc523acc01
João Vitor da Cunha Silveira <148132536+joaoCSilveira@users.noreply.github.com>
2023-11-14 11:31:01 -0300

    Update dbConnnect.js

--- models/dbConnnect.js
+++ models/dbConnnect.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise")
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "kj.sim2!220",
-    database: "bank",
-    port: 3306
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DBPORT
 })
 
 module.exports = db
